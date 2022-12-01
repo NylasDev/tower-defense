60001f5decddce381e56f29577c83bcc90d99e82
Sergiu Vataman <sergiuvataman.dev@gmail.com>
2022-12-01 15:10:29 +0200

    working on building placement

--- js/index.js
+++ js/index.js
@@ -9,51 +9,35 @@ canvas.height = 768;
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const buildTiles2D = [];
+for (let i = 0; i < buildTiles.length; i += 20) {
+  buildTiles2D.push(buildTiles.slice(i, i + 20));
+}
+
+const placementTiles = [];
+
+// console.log(buildTiles2D);
+buildTiles2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 14) {
+      //add building placement tile here
+      placementTiles.push(
+        new PlacementTile({
+          position: {
+            x: x * 64,
+            y: y * 64,
+          },
+        })
+      );
+    }
+  });
+});
+console.log(placementTiles);
+
 //map draw
 const image = new Image();
 image.onload = () => {};
 image.src = "img/gameMap.png";
-
-class Enemy {
-  constructor({ position = { x: 0, y: 0 } }) {
-    this.position = position;
-    this.width = 100;
-    this.height = 100;
-    this.waypointIndex = 0;
-    this.center = {
-      x: this.position.x + this.width / 2,
-      y: this.position.y + this.height / 2,
-    };
-  }
-  draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-
-  update() {
-    this.draw();
-
-    //movement
-    const waypoint = waypoints[this.waypointIndex];
-    const xDistance = waypoint.x - this.center.x;
-    const yDistance = waypoint.y - this.center.y;
-    const angle = Math.atan2(yDistance, xDistance);
-    this.position.x += Math.cos(angle);
-    this.position.y += Math.sin(angle);
-    this.center = {
-      x: this.position.x + this.width / 2,
-      y: this.position.y + this.height / 2,
-    };
-
-    if (
-      Math.round(this.center.x) === Math.round(waypoint.x) &&
-      Math.round(this.center.y) === Math.round(waypoint.y) &&
-      this.waypointIndex < waypoints.length - 1
-    ) {
-      this.waypointIndex++;
-    }
-  }
-}
 
 const enemies = [];
 for (let i = 1; i < 10; i++) {
@@ -65,6 +49,9 @@ for (let i = 1; i < 10; i++) {
   );
 }
 
+const buildings = [];
+let activeTile = undefined;
+
 function animate() {
   requestAnimationFrame(animate);
 
@@ -72,6 +59,18 @@ function animate() {
   enemies.forEach((enemy) => {
     enemy.update();
   });
+
+  placementTiles.forEach((tile) => {
+    tile.update(mouse);
+  });
 }
 
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
 animate();
