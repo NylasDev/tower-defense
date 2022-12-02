c5db5ec37163164c60cab6b8f0b880e29e228ec7
Sergiu Vataman <sergiuvataman.dev@gmail.com>
2022-12-02 13:48:49 +0200

    health bar

--- js/index.js
+++ js/index.js
@@ -54,9 +54,11 @@ function animate() {
   requestAnimationFrame(animate);
 
   c.drawImage(image, 0, 0);
-  enemies.forEach((enemy) => {
+
+  for (let i = enemies.length - 1; i >= 0; i--) {
+    const enemy = enemies[i];
     enemy.update();
-  });
+  }
 
   placementTiles.forEach((tile) => {
     tile.update(mouse);
@@ -81,7 +83,19 @@ function animate() {
       const xDiffrence = projectile.enemy.center.x - projectile.position.x;
       const yDiffrence = projectile.enemy.center.y - projectile.position.y;
       const distance = Math.hypot(xDiffrence, yDiffrence);
+
+      //projectile hits
       if (distance < projectile.enemy.radius + projectile.radius) {
+        projectile.enemy.health -= 20;
+        if (projectile.enemy.health <= 0) {
+          const enemyIndex = enemies.findIndex((enemy) => {
+            return projectile.enemy === enemy;
+          });
+          if (enemyIndex > -1) {
+            enemies.splice(enemyIndex, 1);
+          }
+        }
+        console.log(projectile.enemy.health);
         building.projectiles.splice(i, 1);
       }
       // console.log(xDiffrence);
